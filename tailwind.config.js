137d8dba9d846f76f618b4fca37cfd687ab2a729
unknown <guilhermedrumond@gmail.com>
2023-01-05 09:07:11 -0300

    Inclusão de border radius

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      borderRadius: {
+        'default': '20px',
+      },
+    },
+    plugins: [],
+  }
 }
